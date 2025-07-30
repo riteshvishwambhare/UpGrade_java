4c8fd8b1c449c6a7dba389cf10a21ee782cfa06e
riteshvishwambhare <riteshvishwambhare@gmail.com>
2025-07-30 18:01:27 +0530

    First commit

---
+++ first.java
@@ -0,0 +1,6 @@
+public class first {
+    public static void main(String[] args){
+        system.out.println("hello from the first file!");
+        system.out.println("calling second file into main method");
+    }
+}
